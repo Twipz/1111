8cc426e921e9187a9e88f5e99dceb5d8d405eb0e
Twipz <darkgroup98@mail.ru>
2018-05-08 12:55:46 +0700

    Adding chessboard

--- main.c
+++ main.c
@@ -1,4 +1,9 @@
 #include <stdlib.h>
 
 int main () {
+	printf("8 r n b q k b n r\n");
+	printf("7 p p p p p p p p\n");
+	printf("6 \n5 \n 4 \n3 \n");
+	printf("2 P P P P P P P P");
+	printf("1 R N B Q K B N R");
 }
